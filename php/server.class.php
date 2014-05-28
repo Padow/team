87f4b27a9f26977fb523de452c8edbf2b1e5aa27
Padow <patrick.douchement@gmail.com>
2014-05-28 23:53:00 +0200

    correction sever.class.php

--- php/server.class.php
+++ php/server.class.php
@@ -8,23 +8,34 @@
         $key = array('IP', 'Port', 'Password');
         foreach ($key as $value) {
           if(in_array($value, $array_keys)){
-            $verif = true;
+            $verifkey = true;
           }else{
-            $verif = false;
+            $verifkey = false;
             break;
           }
-        } 
-        if($verif){
-          echo '<p><a href="steam://connect/'; 
-          echo $server_array['IP'].":";
-          echo $server_array['Port']."/";
-          echo $server_array['Password']; 
-          echo'" class="btn btn-default btn-block"><span class="glyphicon glyphicon-fire"></span> Connect</a></p>';
-
-          echo "<p> connect ".$server_array['IP'].":".$server_array['Port']."/".$server_array['Password']."</p>";
         }
-        
-            
+
+        if($verifkey){
+
+          foreach ($server_array as  $value) {
+            if($value !="")
+              $verifvalue = true;
+            else{
+              $verifvalue = false;
+              break;
+            }
+          }
+
+          if($verifvalue){
+            echo '<p><a href="steam://connect/'; 
+            echo $server_array['IP'].":";
+            echo $server_array['Port']."/";
+            echo $server_array['Password']; 
+            echo'" class="btn btn-default btn-block"><span class="glyphicon glyphicon-fire"></span> Connect</a></p>';
+
+            echo "<p> connect ".$server_array['IP'].":".$server_array['Port']."/".$server_array['Password']."</p>";
+          } 
+        }      
       }
     }  
  }
