450389a5ff5dedfc5353689e6ee820b5f274bc54
Padow <patrick.douchement@gmail.com>
2014-06-16 16:03:41 +0200

    fix language bug

--- php/language.class.php
+++ php/language.class.php
@@ -8,6 +8,18 @@
 			$this->_connexion = parent::__construct();
 		}
 
+		public function encrypt($data) {
+		    $key = "eryzing";  // Clé de 8 caractères max
+		    $data = serialize($data);
+		    $td = mcrypt_module_open(MCRYPT_DES,"",MCRYPT_MODE_ECB,"");
+		    $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+		    mcrypt_generic_init($td,$key,$iv);
+		    $data = base64_encode(mcrypt_generic($td, '!'.$data));
+		    mcrypt_generic_deinit($td);
+		    $this->_encrypt = $data;
+		    return $this->_encrypt;
+		}
+
 		public function getLanguage(){
 			$dir = __DIR__;
 
@@ -38,7 +50,8 @@
 			$sql-> bindParam('name', $name, PDO::PARAM_STR);
 			$sql-> bindParam('language', $lang, PDO::PARAM_STR);
 			$sql-> execute();
-			setcookie("__rmblp", $lang, time()+365*24*3600,'/');
+			$lang_cookie = $this->encrypt($lang);
+			setcookie("__rmblp", $lang_cookie, time()+365*24*3600,'/');
 			echo '
 					<script type="text/javascript">
                           window.location.href="player_setting.php";
